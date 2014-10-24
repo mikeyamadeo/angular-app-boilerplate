d85246dec6c03bdb5d0994617e59c1fc5bb1b8b4
Mikey Murphy <mikeyamadeo@gmail.com>
2014-10-23 18:44:43 -0600

    main module that ties the app together

---
+++ src/app/app.module.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular.module('app', [
+
+        /*
+         * shared
+         */
+        'app.core'
+
+        /*
+         * features
+         */
+    ]);
+
+})();
