d4a0e7962deca22518e9f938b3180cbf2f5ccb0f
Mikey Murphy <mikeyamadeo@gmail.com>
2014-10-23 18:50:30 -0600

    module used to house core, ng, general, and other 3rd party modules

---
+++ src/app/core/core.module.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict'
+
+    angular.module('app.core', [
+        /*
+         * Angular modules
+         */
+
+        /*
+         * Reusable cross app code
+         */
+
+        /*
+         * 3rd party
+         */
+
+        'ui.router'
+    ]);
+
+})();
