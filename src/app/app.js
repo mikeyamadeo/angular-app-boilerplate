5711ec09ab01d96a285099d4bbc4c896d011711a
Mikey Murphy <mikeyamadeo@gmail.com>
2014-10-28 08:41:10 -0600

    + IIFE skeleton for a base controller

---
+++ src/app/app.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('baseCtrl', baseCtrl);
+
+    baseCtrl.$inject = [ '$scope' ];
+
+    function baseCtrl( $scope ) {
+
+        
+
+    }
+
+})();
