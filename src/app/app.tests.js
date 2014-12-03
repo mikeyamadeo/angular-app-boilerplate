f49f5f48cd4e4224acd89dd9ba19a94eed977ad6
Mikey Murphy <mikeyamadeo@gmail.com>
2014-12-03 10:39:46 -0700

    + testing appCtrl's instantiating of properties

--- src/app/app.tests.js
+++ src/app/app.tests.js
@@ -1,16 +1,12 @@
-describe('', function() {
-    var currentUser;
+describe('appCtrl', function() {
+    var ctrl;
 
     beforeEach(module('app'));
-    beforeEach(inject(function($injector) {
-        // Job = $injector.get('Job');
-        // appCtrl = $injector.get('appCtrl');
-        // appCtrl = $injector.get('appCtrl');
+    beforeEach(inject(function($controller) {
+        ctrl = $controller('appCtrl', {});
     }));
 
-    it('Should assign a value to currentUser', function() {
-        // expect(appCtrl.currentUser).toBe(null);
-        // console.log(Job.name);
-        expect(0).toBe(0);
+    it('should instantiate properties', function() {
+        expect(ctrl.message).not.toBeUndefined();
     });
 })
