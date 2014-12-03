267a9df5efe1d3829310e22a271605abae82dfdd
Mikey Murphy <mikeyamadeo@gmail.com>
2014-12-02 20:41:32 -0700

    + testing script for appCtrl

---
+++ src/app/app.tests.js
@@ -0,0 +1,16 @@
+describe('', function() {
+    var currentUser;
+
+    beforeEach(module('app'));
+    beforeEach(inject(function($injector) {
+        // Job = $injector.get('Job');
+        // appCtrl = $injector.get('appCtrl');
+        // appCtrl = $injector.get('appCtrl');
+    }));
+
+    it('Should assign a value to currentUser', function() {
+        // expect(appCtrl.currentUser).toBe(null);
+        // console.log(Job.name);
+        expect(0).toBe(0);
+    });
+})
